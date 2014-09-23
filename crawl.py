8c9adb99a4e12d68886f80ae373aaaac51d55f62
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-09-23 18:13:37 +0200

    Added seasonal words crawling.

--- crawl.py
+++ crawl.py
@@ -35,5 +35,43 @@ def crawl_haikus():
     with open(OUTPUT_FILENAME, 'w') as output_file:
         json.dump(haikus_dataset, output_file)
 
+    print ''
+    print "Saved to '{}'.".format(OUTPUT_FILENAME)
+
+def crawl_seasonal_words():
+    WORDS_URL = 'http://www.2hweb.net/haikai/renku/500ESWd.html'
+    SEASONAL_FILENAME = 'seasonal.json'
+
+    tree = lxml.html.parse(WORDS_URL)
+
+    dataset = dict()
+    last_title = None
+    for paragraph in tree.xpath('//p'):
+        bold_elements = paragraph.xpath('b')
+        if bold_elements and '--' in bold_elements[0].text_content():
+            if last_title is not None:
+                print "    > {} words".format(len(dataset[last_title]))
+            last_title = bold_elements[0].text_content()
+            dataset[last_title] = list()
+            print ". Parsing '{}'".format(last_title)
+
+        if last_title is None:
+            continue
+
+        if paragraph.xpath('i') and paragraph.xpath('a'):
+            text = paragraph.text_content()
+            text = text.split('(')[0].replace('*', '').replace('[', '').replace(']', '')
+            tokens = text.split('/')
+            for token in tokens:
+                dataset[last_title].append(token.strip())
+    print "    > Parsed {} seasonal words".format(len(dataset[last_title]))
+
+    with open(SEASONAL_FILENAME, 'w') as output_file:
+        json.dump(dataset, output_file)
+
+    print ''
+    print "Saved to '{}'.".format(SEASONAL_FILENAME)
+
 if __name__ == '__main__':
     crawl_haikus()
+    crawl_seasonal_words()
